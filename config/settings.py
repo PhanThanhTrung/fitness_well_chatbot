50de270ff7a8fdf32362416ce32695278ddaa27f
PhanThanhTrung <trung99nb@gmail.com>
2024-03-17 11:16:15 +0700

    update settings

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
     'chat.apps.ChatConfig',
+    'theme.apps.ThemeConfig'
 ]
 
 MIDDLEWARE = [
