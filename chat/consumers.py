b46bedcbfe80d441df17be9bcff8bca9db45976d
PhanThanhTrung <trung99nb@gmail.com>
2024-03-31 22:21:00 +0700

    integrate openai assistant

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,29 +2,26 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-
+from .chatbot import Assistant
 class ChatConsumer(WebsocketConsumer):
+    _bot = Assistant()
+    
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-
-        async_to_sync(self.channel_layer.send)(
-            self.channel_name,
-            {
+        self._bot.talk_to_assistant(input_text=text_data_json["text"])
+        get_event = {
                 "type": "chat_message",
                 "text": {"msg": text_data_json["text"], "source": "user"},
-            },
-        )
+            }
+        self.chat_message(event=get_event)
 
-        async_to_sync(self.channel_layer.send)(
-            self.channel_name,
-            {
-                "type": "chat.message",
-                "text": {"msg": "Bot says hello", "source": "bot"},
-            },
-        )
+        output = self._bot.get_assistant_response()
+        send_event = {
+            "type": "chat.message",
+            "text": {"msg": output, "source": "bot"},
+        }
+        self.chat_message(event=send_event)
 
-    # Handles the chat.mesage event i.e. receives messages from the channel layer
-    # and sends it back to the client.
     def chat_message(self, event):
         text = event["text"]
         self.send(text_data=json.dumps({"text": text}))
