ce4c0a91483231f50e2d140212bce3f13768779d
PhanThanhTrung <trung99nb@gmail.com>
2024-04-28 10:52:52 +0700

    add: tables for management

---
+++ managements/models/exercises.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+class Exercise(models.Model):
+    name = models.CharField(max_length= 200, blank=False)
+    exercise_type = models.CharField(max_length=200, blank=True)
+    main_muscle = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now())
+    updated_at = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+    
