fb47093ab27f010fe94f556f0d57f0c65edcd67b
PhanThanhTrung <trung99nb@gmail.com>
2024-04-28 18:55:18 +0700

    update: model exercise with more details

--- managements/models/exercises.py
+++ managements/models/exercises.py
@@ -7,6 +7,8 @@ class Exercise(models.Model):
     main_muscle = models.TextField()
     created_at = models.DateTimeField(default=timezone.now())
     updated_at = models.DateTimeField(auto_now=True)
+    tools_required = models.CharField(max_length=200, blank=True)
+    tier = models.CharField(max_length=10, blank=True)
     description = models.TextField()
 
     def __str__(self) -> str:
