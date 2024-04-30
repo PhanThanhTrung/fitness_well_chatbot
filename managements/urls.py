07567ffce7e47b45f298b9cdb0f15d029f28aaf3
PhanThanhTrung <trung99nb@gmail.com>
2024-04-30 12:00:54 +0700

    fix: some minor typo. please ignore this

--- managements/urls.py
+++ managements/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 from .views import ExercisesViewSet
 from rest_framework import routers
 
+app_name="management"
 router = routers.DefaultRouter()
-router.register(prefix=r"exercises/", viewset=ExercisesViewSet, basename="exercise")
+router.register(prefix=r"exercises", viewset=ExercisesViewSet, basename="exercise")
 
 urlpatterns = [
     path('', include(router.urls)),
